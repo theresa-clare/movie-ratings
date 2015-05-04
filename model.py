4633523bddc46e8aa715736a40da635629aa1ca4
Hackbright Student <info@hackbrightacademy.com>
2015-05-04 11:20:47 -0700

    Added User, Movie, and Rating classes in model.py

--- model.py
+++ model.py
@@ -12,7 +12,36 @@ db = SQLAlchemy()
 ##############################################################################
 # Model definitions
 
-# Delete this line and put your User/Movie/Ratings model classes here.
+class User(db.Model):
+    """User of ratings website."""
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    email = db.Column(db.String(64), nullable=True)
+    password = db.Column(db.String(64), nullable=True)
+    age = db.Column(db.Integer, nullable=True)
+    zipcode = db.Column(db.String(15), nullable=True)
+
+
+class Movie(db.Model):
+
+    __tablename__ = "movies"
+
+    movie_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    title = db.Column(db.String(64))
+    released_at = db.Column(db.DateTime)
+    imdb_url = db.Column(db.String(100))
+
+
+class Rating(db.Model):
+
+    __tablename__ = "ratings"
+
+    rating_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    movie_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer)
+    score = db.Column(db.Integer)
 
 
 ##############################################################################
