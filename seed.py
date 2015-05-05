49dbe088ed604f90d9497553bac1c103b1ed0a56
Hackbright Student <info@hackbrightacademy.com>
2015-05-04 17:42:55 -0700

    Fixed movie title bug in load_movies in seed.py and changed file reserved name

--- seed.py
+++ seed.py
@@ -7,9 +7,9 @@ from datetime import datetime, date
 def load_users():
     """Load users from u.user into database."""
 
-    file = open("seed_data/u.user")
+    user_file = open("seed_data/u.user")
 
-    for line in file:
+    for line in user_file:
         line_list = line.strip("\n").split("|")
         new_user = User(user_id=line_list[0], age=line_list[1], zipcode=line_list[4])
         db.session.add(new_user)
@@ -20,13 +20,13 @@ def load_users():
 def load_movies():
     """Load movies from u.item into database."""
 
-    file = open("seed_data/u.item")
+    movie_file = open("seed_data/u.item")
 
-    for line in file:
+    for line in movie_file:
         line_list = line.strip("\n").split("|")
 
-        imdb_url_with_year = line_list[4]
-        imdb_url = imdb_url_with_year[:len(imdb_url_with_year)-6]
+        title_with_year = line_list[1]
+        title = title_with_year[:len(title_with_year)-7]
 
         date_list = line_list[2]
         date_object = None
@@ -36,7 +36,7 @@ def load_movies():
         else:
             date_object = None
 
-        new_movie = Movie(movie_id=line_list[0], title=line_list[1], released_at=date_object, imdb_url=imdb_url)
+        new_movie = Movie(movie_id=line_list[0], title=title, released_at=date_object, imdb_url=line_list[4])
 
         db.session.add(new_movie)
 
@@ -44,9 +44,9 @@ def load_movies():
 
 def load_ratings():
     """Load ratings from u.data into database."""
-    file = open("seed_data/u.data")
+    ratings_file = open("seed_data/u.data")
 
-    for line in file:
+    for line in ratings_file:
         line_list = line.strip("\n").split("\t")
         new_rating = Rating(user_id=line_list[0], movie_id=line_list[1], score=line_list[2])
         db.session.add(new_rating)
